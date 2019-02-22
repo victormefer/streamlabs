225d02ae03680f8628eb5c8c481b45314d444596
Victor Ferreira <victor.ferreira@hexgis.com>
2019-02-22 10:49:05 -0300

    FIxed access token, getting user data

--- events/views.py
+++ events/views.py
@@ -5,21 +5,30 @@ import requests
 
 def events(request):
     streamer = request.POST.get('favorite')
+    token = request.POST.get('token')
 
-    # get streamer user id
-    req = requests.get('https//api.twitch.tv/helix/users', {
-        'login': streamer
-    })
-
-    # get user id from request
+    # get streamer user info
+    params = {'login': streamer}
+    headers = {'Authorization': 'Bearer %s' % token}
+    user_req = requests.get('https://api.twitch.tv/helix/users', params=params,
+                            headers=headers)
+    user = user_req.json()['data'][0]
 
     data = {
         'callback': '',
         'mode': 'subscribe',
-        'topic': ''
+        'lease_seconds': '864000',
+        'topic': 'https://api.twitch.tv/helix/users/follows?first=1&to_id={}'
+                 .format(user['id'])
     }
-    req = requests.post('https://api.twitch.tv/helix/webhooks/hub',
-        'https://api.twitch.tv/helix/users/follows?first=1&from_id={}'.format(user.id))
+    req = requests.post('https://api.twitch.tv/helix/webhooks/hub', data=data)
 
     context = {'streamer': streamer}
     return render(request, 'events/events.html', context=context)
+
+
+# def verify_subscription
+
+
+# def get_user_follows(request):
+#     request.
--- login/views.py
+++ login/views.py
@@ -2,12 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
 
+import requests
+
 
 def login(request):
-    token = request.GET.get('code')
-    if token:
-        context = {'token': token}
-        return render(request, 'login/login.html', context=context)
+    code = request.GET.get('code')
+    if code:
+        data = {
+            'client_id': settings.TWITCH_CLIENT_ID,
+            'client_secret': settings.TWITCH_CLIENT_SECRET,
+            'redirect_uri': 'http://localhost:8000',
+            'code': code,
+            'grant_type': 'authorization_code',
+        }
+        token_request = requests.post('https://id.twitch.tv/oauth2/token', data=data)
+        response = token_request.json()
+
+        return render(request, 'login/login.html', context=response)
+    # import pdb;pdb.set_trace()
+    # token = request.GET.get('access_token')
+    # if token:
+    #     context = {'token': token}
+    #     return render(request, 'login/login.html', context=context)
     else:
         params = {
             'client_id': settings.TWITCH_CLIENT_ID,
