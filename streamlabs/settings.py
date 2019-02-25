c32b1c38e20a943ec19499d906d1cccb2c983b63
Victor Ferreira <victor.ferreira@hexgis.com>
2019-02-25 11:41:09 -0300

    Updating redis url

--- streamlabs/settings.py
+++ streamlabs/settings.py
@@ -142,7 +142,8 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(BASE_URL, 6379)],
+            'hosts': [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            # "hosts": [(BASE_URL, 6379)],
         },
     },
 }
